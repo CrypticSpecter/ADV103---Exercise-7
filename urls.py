31e38abbce263292a013107cf494a6b9c1bcd1f9
CrypticSpecter <148252338+CrypticSpecter@users.noreply.github.com>
2023-12-12 01:18:07 +0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,12 @@
-"""
-URL configuration for ADVexe2 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import TagListCreateView, TagDetailUpdateDeleteView, CategoryListCreateView, CategoryDetailUpdateDeleteView, ArticleListCreateView, ArticleRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/articles/', include('Exe2.urls')),
-    path('api/', include('Exe2.urls')),
+    path('tags/', TagListCreateView.as_view(), name='tag-list-create'),
+    path('tags/<int:pk>/', TagDetailUpdateDeleteView.as_view(), name='tag-detail-update-delete'),
+    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', CategoryDetailUpdateDeleteView.as_view(), name='category-detail-update-delete'),
+    path('articles/', ArticleListCreateView.as_view(), name='article-list'),
+    path('articles/<int:pk>/', ArticleRetrieveUpdateDestroyView.as_view(), name='article-detail'),
 ]
-
 
